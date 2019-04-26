9080b5e1f3ff4b47e82b38634e579adee2e40859
atlansien <atlansien@yahoo.co.jp>
2019-04-26 21:13:11 +0900

    create/ return_a_value_with_splice.js

---
+++ code/Javascript/return_a_value_with_splice.js
@@ -0,0 +1,9 @@
+const array = ["a", "b", "c", "d"];
+
+const valueInArray = array.splice(0,1);
+
+console.log(valueInArray); // ["a"] 配列で返ってくる。
+
+const value = array.splice(0,1)[0]; // 配列ではなくデータで返して欲しい場合末尾に[0]を加える
+
+console.log(value); // "b" １つのデータとして返ってくる
