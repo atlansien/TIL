80f9db869d11ee4d2f0f75d57c5dbbdd29057e9b
atlansien <atlansien@yahoo.co.jp>
2019-04-28 02:48:41 +0900

    fix & create/ add comment to find_object_matching_id.js & creating writing_only.md

--- code/JavaScript/find_object_matching_id.js
+++ code/JavaScript/find_object_matching_id.js
@@ -1,4 +1,4 @@
-/* create at 2019/04/27*/
+/* create at 2019/04/27 */
 
 const array = [
   { id: 1, data: `data 1` },
@@ -6,8 +6,8 @@ const array = [
   { id: 3, data: `data 3` }
 ];
 
-const id = 1;
+const id = 1; // idの値が「1」のオブジェクトを取り出したい
 
-const target = array.findIndex(obj => id === obj.id);
+const target = array.findIndex(obj => id === obj.id); // idと合致するobj.idを探す、見つかった場合、そのobjのインデックス値を返す
 
 console.log(array[target]);
